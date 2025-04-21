bfc01e485d1f521edf2c35a5914673c052023814
MaratSharshenaliev <sharshenaliev.2003@icloud.com>
2025-04-22 02:29:03 +0600

    add: vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import "@/app/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: "Elordo Group - Лидер в строительстве элитной недвижимости в Бишкеке",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
+          <SpeedInsights/>
         </ThemeProvider>
       </body>
     </html>
